c725aacdd626c91922c8e60ba2b68e00e6b4ff8d
Martin Koegler <martin.koegler@chello.at>
2014-08-09 10:12:27 +0200

    Fix ASLIBEXECDIR

--- cctools/as/driver.c
+++ cctools/as/driver.c
@@ -27,7 +27,7 @@ int argc,
 char **argv,
 char **envp)
 {
-    const char *LIB = "../libexec/as/";
+    const char *LIB = ASLIBEXECDIR;
     const char *LOCALLIB = "../local/libexec/as/";
     const char *AS = "/as";
 
